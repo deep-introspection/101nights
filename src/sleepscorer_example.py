091d19c5b4ce7ec90820c2618ff5872d1200c1db
Guillaume Dumas <deep@introspection.eu>
2018-06-09 18:58:20 +0200

    Almost there with Sleep Scorer

--- src/sleepscorer_example.py
+++ src/sleepscorer_example.py
@@ -1,12 +1,13 @@
 from sleepscorer import Classifier
 import mne
+import numpy as np
 
 path_to_file = '../data/raw/EEG/Nathalie-78_20171118_123017.mff'
 raw = mne.io.read_raw_egi(path_to_file,
                           montage='GSN-HydroCel-256',
                           preload=True)
 
-raw.crop(tmin=3600, tmax=3600*2)
+#raw.crop(tmin=3600, tmax=3600*2)
 raw.filter(0.15, 50., fir_design='firwin')
 raw.resample(100, npad='auto')
 raw.set_eeg_reference('average', projection=True)
@@ -17,7 +18,9 @@ events = mne.event.make_fixed_length_events(
 epochs = mne.epochs.Epochs(raw, events, tmin=0, tmax=30.0,
                            baseline=None, preload=True)
 data = epochs.get_data() # load your python array, preprocessed
-# assert(data.ndim==3 and data.shape[1:]==(3000,3))
+data = data[:,np.array([8, 31, 229]),:3000]
+data = np.transpose(data, (0, 2, 1))
+assert(data.ndim==3 and data.shape[1:]==(3000,3))
 
 clf = Classifier()
 # clf.download_weights()  # skip this if you already downloaded them.
