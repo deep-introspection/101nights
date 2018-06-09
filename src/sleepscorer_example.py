2e40c38ca4edb1a4ea64b33fa972b24e53f4bfac
Guillaume Dumas <deep@introspection.eu>
2018-06-09 18:44:42 +0200

    Update Sleep Scorer

--- src/sleepscorer_example.py
+++ src/sleepscorer_example.py
@@ -1,10 +1,22 @@
 from sleepscorer import Classifier
 import mne
-epochs_clean = mne.read_epochs('../data/derived/cleaned_epo.fif')
-epochs_clean = epochs_clean.filter(0.15, None)
-epochs_clean = epochs_clean.resample(100, npad='auto')
 
-data = epochs_clean.get_data() # load your python array, preprocessed
+path_to_file = '../data/raw/EEG/Nathalie-78_20171118_123017.mff'
+raw = mne.io.read_raw_egi(path_to_file,
+                          montage='GSN-HydroCel-256',
+                          preload=True)
+
+raw.crop(tmin=3600, tmax=3600*2)
+raw.filter(0.15, 50., fir_design='firwin')
+raw.resample(100, npad='auto')
+raw.set_eeg_reference('average', projection=True)
+
+events = mne.event.make_fixed_length_events(
+	raw, id=9999, start=0, stop=None, duration=30.0,
+	first_samp=True)
+epochs = mne.epochs.Epochs(raw, events, tmin=0, tmax=30.0,
+                           baseline=None, preload=True)
+data = epochs.get_data() # load your python array, preprocessed
 # assert(data.ndim==3 and data.shape[1:]==(3000,3))
 
 clf = Classifier()
