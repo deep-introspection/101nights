8c4a7d4436f31def35971401c2867101af9df579
Guillaume Dumas <deep@introspection.eu>
2018-06-09 17:47:21 +0200

    Update word2vec_example.py

--- src/word2vec_example.py
+++ src/word2vec_example.py
@@ -2,8 +2,17 @@ from gensim.models.wrappers import FastText
 
 model = FastText.load_fasttext_format('../data/raw/BIN/wiki.simple')
 
+# Give the embedding of a given word
+print(model.wv['brain'])
+      
+# Test if a word is in the model
+print('brain' in model.wv.vocab)
+
+# Give the most similar words
 print(model.most_similar('brain'))
 
+# Compute similarity between two words
 print(model.similarity('brain', 'synapse'))
 
-print(model.wv.most_similar(positive=['Trump'], negative=['president']))
+# Make arithmetic with words
+print(model.wv.most_similar(positive=['king', 'woman'], negative=['man']))
