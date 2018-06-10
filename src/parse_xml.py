be110254f4020dd2ee8bc3c4f7d3533e37e6447e
Joan Massich <sik@visor.udg.edu>
2018-06-10 09:49:47 +0200

    Add parse_xml.py

---
+++ src/parse_xml.py
@@ -0,0 +1,28 @@
+import os
+from lxml import etree
+import pandas as pd
+from datetime import datetime
+
+def text_to_datetime(time_txt):
+    return datetime(*[int(fields) for fields in time_txt.split("-")])
+
+def build_tuple(message):
+    time, word = message.getchildren()
+    return {"time": text_to_datetime(time.text), "word": word.text, "type": message.get(key="type")}
+
+# def parse_file(fname):
+
+def get_events_from_xml():
+    pathxmls = '../data/raw/XML'
+    files = [fil for fil in os.listdir(pathxmls) if fil[-4:] == ".xml"]
+    df = pd.DataFrame(columns=["time", "word", "type"])
+    for file in files:
+        xmlpath = os.path.join(pathxmls, file)
+        xml = open(xmlpath, "r").read()
+        parser = etree.XMLParser(ns_clean=True, recover=True)
+        tree = etree.fromstring("<data>{0}</data>".format(xml), parser)
+        root = tree.getroottree().getroot()
+        messages = [build_tuple(e) for e in root.getchildren() if e.tag == "message"]
+        for message in messages:
+            df = df.append(message, ignore_index=True)
+    return df
