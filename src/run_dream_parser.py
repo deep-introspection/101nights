7d1c3e1b9828beab885de05cc1acbb704514004b
Ale Abdo <abdo@member.fsf.org>
2018-06-10 15:22:46 +0200

    dream_parser: fix timezones! refactor get dtimed_texts

--- src/run_dream_parser.py
+++ src/run_dream_parser.py
@@ -6,17 +6,29 @@
 
 import re, pandas
 from os import path
-from datetime import time, date, datetime
+from datetime import date, datetime
+from numpy import datetime64, timedelta64
 import pdb # pdb.set_trace()
 
 YEAR = 2017
 GET_MONTH = {'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}.get
 
+def FIX_TIMEZONE(dtimed_texts):
+    """Sets datetimes to UTC"""
+    # this works since there are no records between 01:00 and 02:00
+    tzchange = datetime64('2017-05-02T09:00')
+    tzdelta0, tzdelta1 = timedelta64(7, 'h'), timedelta64(8, 'h')
+    for item in dtimed_texts:
+        if item[1] < tzchange:
+            item[1] += tzdelta0
+        else:
+            item[1] += tzdelta1
+
 def parse_headers(source_file, header_re):
 
     with open(source_file) as f:
         raw_text = f.read()
-    
+
     re_header = re.compile( header_re, re.I )
     headers = re_header.findall(raw_text)
 
@@ -28,34 +40,32 @@ def parse_headers(source_file, header_re):
     
     nights, _, places, months, days = zip(*headers)
     dates = [date(YEAR, GET_MONTH(m), int(d)) for m, d in zip(months, days)]
-    
+
     index = pandas.Index(list(map(int, nights)), name='night')
     dict_ = dict( place=places, date=dates, text=texts )
-    
+
     return pandas.DataFrame(dict_, index=index)
 
 def parse_times(df):
     time_re = '\n*(\d{1,2}):(\d{1,2}) ?(.*)'
     re_time = re.compile( time_re )
 
-    timesliced = df['text'].transform(lambda x: tuple(re_time.finditer(x)))
-    dtimed_texts = [ (idx, # current index
-                     datetime( df.loc[idx, 'date'].year, df.loc[idx, 'date'].month,
-                               df.loc[idx, 'date'].day, int(ts[1]), int(ts[2]) ),
-                     ts[3]) # timed text
-                    for idx, slices in timesliced.iteritems()
-                    for ts in slices if ts ]
+    dtimed_texts = []
+    for item in df.itertuples():
+        slices = (x.groups() for x in re_time.finditer(item.text) if x)
+        for hour, minute, text in slices:
+            dt = datetime64(datetime(item.date.year, item.date.month,
+                            item.date.day, int(hour), int(minute)))
+            dtimed_texts.append( [item.Index, dt, text] )
 
-    major_index = df.index.to_series()
-    new_index = pandas.MultiIndex.from_tuples(
-        [ (major_index.loc[i], dt) for i, dt, _ in dtimed_texts ],
-        names=[df.index.name, 'time'] )
+    FIX_TIMEZONE(dtimed_texts)
+
+    new_index = pandas.Index( [dt for _, dt, _ in dtimed_texts] )
 
     return pandas.DataFrame(
        ( [df.loc[i, 'place'], txt] for i, _, txt in dtimed_texts ),
         columns=['place', 'text'],
-        index=new_index
-    )
+        index=new_index )
 
 def __main__():
 
@@ -81,6 +91,8 @@ def __main__():
     dreams = parse_headers(dream_file, dream_re)
     diary = parse_headers(diary_file, diary_re)
     tdiary = parse_times(diary)
+
+    #import pdb; pdb.set_trace()
 
     # save to files
     dreams.to_csv(dream_out)
