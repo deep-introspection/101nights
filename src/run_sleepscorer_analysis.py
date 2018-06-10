be13e67a6518ff7aadff05627142d42fc09495d3
Guillaume Dumas <deep@introspection.eu>
2018-06-10 11:54:37 +0200

    Enhance Sleep Scorer preprocessing and formatting

--- src/run_sleepscorer_analysis.py
+++ src/run_sleepscorer_analysis.py
@@ -9,10 +9,26 @@ np.random.seed(42)
 
 # Load cleaned epochs
 epochs_clean = mne.read_epochs('../data/derived/cleaned_sleep_scorer_epo.fif')
+# epochs_clean.plot_sensors(show_names=True)
+
+
+# Pick a channel for EEG, EMG, EOG
+# picks = mne.pick_types(epochs_clean.info, eeg=True, exclude='bads')
+epochs_clean.pick_channels(['E16', 'E229', 'E31'])
+epochs_clean.set_eeg_reference('average', projection=False)
+epochs_clean.set_channel_types({'E16': 'eeg'})
+epochs_clean.set_channel_types({'E229': 'emg'})
+epochs_clean.set_channel_types({'E31': 'eog'})
+epochs_clean.reorder_channels(['E16', 'E229', 'E31'])
+
+# filter EMG >10Hz
+picks = mne.pick_types(epochs_clean.info, emg=True, eeg=False, eog=False,
+                       stim=False, exclude='bads')
+epochs_clean.filter(10., None, fir_design='firwin', picks=picks)
 
 # Format for Sleep Scorer
 data = epochs_clean.get_data() # load your python array, preprocessed
-data = data[:,np.array([8, 31, 229]),:3000]
+data = data[:,:,:3000]
 data = np.transpose(data, (0, 2, 1))
 assert(data.ndim==3 and data.shape[1:]==(3000,3))
 
